1559eae555e228bf5553184fdf671259e1acd859
Sigurður Snær Eiríksson <sse1987@gmail.com>
2014-03-18 23:52:53 +0000

    So close, yet so far!

--- js/src/answerCtrl.js
+++ js/src/answerCtrl.js
@@ -15,12 +15,74 @@ function ($scope, $location, $routeParams, StudentFactory) {
 		console.log("Error fetching evaluation: " + errorMessage);
 	});
 
-	var evalData = "Eval data needs to go in here";
-
 	$scope.submitEval = function () {
-		StudentFactory.pushval(course, semester, ID, evalData).then(function (data) {
+		
+		var i, k;
+		var evalAnswers = [];	
+		var evaluation = $scope.evaluation;
+		
+		console.log("evaluation: ");
+		console.log(evaluation);
+		
+		for (i = 0; i < evaluation.CourseQuestions.length; i++) {
+			
+			if (evaluation.CourseQuestions[i].givenAnswer !== undefined) {
+				
+				console.log("C1 Spurning: " + evaluation.CourseQuestions[i].ID + ":" + evaluation.CourseQuestions[i].givenAnswer);
+				evalAnswers.push({
+					QuestionID: evaluation.CourseQuestions[i].ID,
+					Value: evaluation.CourseQuestions[i].givenAnswer
+				});
+			}
+			else
+			{
+				for (k = 0; k < evaluation.CourseQuestions[i].Answers.length; k++) {
+					
+					if (evaluation.CourseQuestions[i].Answers[k].givenAnswer !== undefined) {
+						
+						console.log("C2 Spurning: " + evaluation.CourseQuestions[i].ID + ":" + evaluation.CourseQuestions[i].givenAnswer);
+						evalAnswers.push({
+							QuestionID: evaluation.CourseQuestions[i].ID,
+							Value: evaluation.CourseQuestions[i].Answers[k].givenAnswer
+						});
+					}
+					
+				}
+			}
+			
+		}
+		
+		for (i = 0; i < evaluation.TeacherQuestions.length; i++) {
+			
+			if (evaluation.TeacherQuestions[i].givenAnswer !== undefined) {
+				
+				console.log("T1 Spurning: " + evaluation.TeacherQuestions[i].ID + ":" + evaluation.TeacherQuestions[i].givenAnswer);
+				evalAnswers.push({
+					QuestionID: evaluation.TeacherQuestions[i].ID,
+					Value: evaluation.TeacherQuestions[i].givenAnswer
+				});
+			}
+			else
+			{
+				for (k = 0; k < evaluation.TeacherQuestions[i].Answers.length; k++) {
+					
+					if (evaluation.TeacherQuestions[i].Answers[k].givenAnswer !== undefined) {
+						
+						console.log("T2 Spurning: " + evaluation.TeacherQuestions[i].ID + ":" + evaluation.TeacherQuestions[i].givenAnswer);
+						evalAnswers.push({
+							QuestionID: evaluation.TeacherQuestions[i].ID,
+							Value: evaluation.TeacherQuestions[i].Answers[k].givenAnswer
+						});
+					}
+					
+				}
+			}
+			
+		}
+		
+		/*StudentFactory.pushval(course, semester, ID, evalData).then(function (data) {
 			$location.path("/student");
-		});
+		});*/
 	};
 
 }]);
